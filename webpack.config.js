2c1b5e984a7d0ca08430289b0e9a688833b2feac
Shoto <mrkyloren117@gmail.com>
2020-03-15 00:54:04 -0600

    Configuracion de webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path")
+
+const HtmlWebPackPluging = require('html-webpack-plugin')
+
+module.exports = {
+    mode: "development",
+    entry: {
+        home: path.resolve(__dirname, './src/index.js')
+    },
+    output:{
+        path: path.resolve(__dirname, "dist"),
+        filename: "output.bundle.js"
+    },
+    devServer: {
+        hot: true,
+        open: true,
+        port: 5050
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPluging({
+            inject: true,
+            template: "./index.html", //De donde lo toma
+            filename: "./index.html" //Lo pega en el dist con ese nombre
+        })
+    ]
+}
